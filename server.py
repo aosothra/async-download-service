983ab591a37f9b7690c5c5d564cad3c1ee18c3cd
aosothra <12795376+aosothra@users.noreply.github.com>
2022-07-29 00:40:00 +0400

    stream zip archives

--- server.py
+++ server.py
@@ -1,9 +1,56 @@
-from aiohttp import web
 import aiofiles
+import asyncio
+import logging
+import os
+import pathlib
+
+from aiohttp import web
+from argparse import ArgumentParser
+from asyncio import create_subprocess_exec, subprocess
+
+
+throttle_tick_time = 0
+photos_dir = 'test_photos'
+logger = logging.getLogger(__file__)
 
 
 async def archive(request):
-    raise NotImplementedError
+    global throttle_tick_time, photos_dir
+
+    archive_hash = request.match_info['archive_hash']
+
+    if not os.path.exists(os.path.join(photos_dir, archive_hash)):
+        raise web.HTTPNotFound(text=f'404: Archive {archive_hash} does not exist.')
+
+    response = web.StreamResponse(
+        headers={
+            'Content-Disposition': f'attachment; filename="{archive_hash}.zip"',
+            'Content-Type': 'application/zip'
+        }
+    )
+
+    await response.prepare(request)
+
+    exec = 'zip'
+    args = ['-rq', '-', archive_hash]
+    process = await create_subprocess_exec(exec, *args, stdout=subprocess.PIPE, cwd=photos_dir)
+
+    try:
+        while True:
+            data = await process.stdout.read(512000)
+            logger.info(f'Sending archive chunk {archive_hash}({len(data)})')
+
+            if throttle_tick_time:
+                await asyncio.sleep(throttle_tick_time)
+
+            await response.write(data)
+            if process.stdout.at_eof():
+                break
+        
+        await response.write_eof()
+    finally:
+        process.kill()
+        logger.info(f'Download was interrupted.')
 
 
 async def handle_index_page(request):
@@ -13,6 +60,18 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Flag for verbose output')
+    parser.add_argument('-t', '--throttle_tick', type=int, default=0, help='Amount of forced delay on stream response iteration')
+    parser.add_argument('-p', '--photo_dir', type=pathlib.Path, default='test_photos', help='Path to photo archive directory')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.ERROR)
+    if args.verbose:
+        logger.setLevel(logging.INFO)
+    throttle_tick_time = args.throttle_tick
+    photos_dir = args.photo_dir
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
